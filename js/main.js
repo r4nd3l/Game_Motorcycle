3ac44b941b4d4ad6c2f33a384f0ce9ebacca2b7c
matemolnar88 <matemolnar88@gmail.com>
2020-01-04 00:05:11 +0100

    Update in engine

--- js/main.js
+++ js/main.js
@@ -28,43 +28,43 @@ function loop(){
   for (let i = 0; i < c.width; i++)
     ctx.lineTo(i, noise(i));
 
-  ctx.fill();
-  requestAnimationFrame(loop);
+var p2 = c.height - noise(t+5 + this.x) * 0.25;
+// 32
+var grounded = 0;
+if(p1-15 > this.y){
+  this.ySpeed += 0.1;
+}else{
+  this.ySpeed -= this.y - (p1-15);
+  this.y = p1 - 15;
+  grounded = 1;
 }
 
-loop();
+if(!playing || grounded && Math.abs(this.rot) > Math.PI * 0.5){
+  playing = false;
+  this.rSpeed = 5;
+  k.ArrowUp = 1;
+  this.x -= speed * 2.5;
+}
 
+var angle = Math.atan2((p2-15) - this.y, (this.x+5) - this.x);
+this.y += this.ySpeed;
 
+if(grounded){
+  this.rot -= (this.rot - angle) * 0.5;
+  this.rSpeed = this.rSpeed - (angle - this.rot);
+}
+this.rSpeed += (k.ArrowLeft - k.ArrowRight) * 0.05;
+this.rot -= this.rSpeed * 0.1;
+if(this.rot > Math.PI) this.rot = -Math.PI;
+if(this.rot < -Math.PI) this.rot = Math.PI;
+ctx.save();
+ctx.translate(this.x, this.y);
+ctx.rotate(this.rot);
+ctx.drawImage(this.img, -15, -15, 30, 30);
+ctx.restore();
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// 67
 var t = 0;
 var speed = 0;
 var playing = true;
